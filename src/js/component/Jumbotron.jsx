f718622b7f93f1f31bf3ae8f8e618acb44d6cf97
Bforti <139017911+Bforti@users.noreply.github.com>
2023-09-16 02:49:45 +0000

    commit

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+const Jumbotron =()=>{return(
+    <div className="p-4 bg-light">
+    <h1>a warm welcome</h1>
+    <p>lore</p>
+    <button className="btn btn-primary btn-lg">Call to action!!</button></div>
+
+);}
+
+
+export default Jumbotron
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-
+import Navbar from "./Navbar.jsx"
+import Jumbotron from "./Jumbotron.jsx"
+import Card from "./Card"
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const dataCards=[{tittle: "first", content="lorem-picsum" },
+                 	{tittle: "second", content="lorem-picsum" },
+                 	{tittle: "third", content="lorem-picsum" },
+                 	{tittle: "four", content="lorem-picsum" }
+
+                      ]
+	return (<>
+		<Navbar/>
+		<div className="container bg-"><Jumbotron/></div>
+		<div className="container text-center">
+			<div className ="row">
+				{dataCards.map((cardObject,index)=>{
+					return(
+						<Card title 
+						/>
+					);
+				}
+				)}
+				<div>
+			</div>
+		</>
+		);
 };
 
 export default Home;
